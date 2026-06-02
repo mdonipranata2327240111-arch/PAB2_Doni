22d5813fc72c36ea29ccf9a77d0bb3d719bb1e0e
Balzedd <syakiranaciffakhairunissa_2327240114@mhs.mdp.ac.id>
2026-06-02 15:11:44 +0700

    tes

--- notes/android/app/build.gradle.kts
+++ notes/android/app/build.gradle.kts
@@ -24,11 +24,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.notes"
+        applicationId = "com.gold.notes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
